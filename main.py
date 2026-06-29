a38fac106c5ef08d101fa101d3bf804a0c376383
Hajar Moukit <hajarmoukit0x5@gmail.com>
2026-06-29 13:55:39 +0100

    adding problem6 files

--- main.py
+++ main.py
@@ -182,5 +182,46 @@
 #     print(result["trackName"])
 
 
+# name = input("What's your name?" )
+# print(f"hello, {name}")
 
+# names = []
+
+# for _ in range(3):
+#     name = input("What's your name?" )
+#     names.append(name)
     
+# for name in sorted(names):
+#     print(name)
+
+# name = input("what is your name? ")
+# file = open("test.txt", "a")
+# file.write(f"{name}\n")
+# file.close()
+
+# with open("names.txt", "r") as file:
+#     lines = file.readlines()
+
+# for line in lines:
+#     print("hello,", line)
+
+# with open("names.txt", "r") as file:
+#     lines = file.readlines()
+
+# for line in lines:
+#     print("hello,", line)
+
+
+import sys
+
+from PIL import Image
+
+images = []
+
+for arg in sys.argv[1:]:
+    image = Image.open(arg)
+    images.append(image)
+
+images[0].save(
+    "costumes.gif", save_all=True, append_images=[images[1]], duration=200, loop=0
+)
